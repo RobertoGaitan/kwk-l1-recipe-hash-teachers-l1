53d9c0a6eca7aac7566d2e6b0af2ad59f1df4df6
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-11 11:11:08 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,27 +1,18 @@
 #code your solutions here
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
-#  - almond flour - 2 cups
-#  - gluten freewhole oats - 1 cup
-#  - kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup
-perfect_10_recipe = {}
-perfect_10_recipe[:almond_flour] = "2 cups"
-perfect_10_recipe[:gluten_free_whole_oats] = "1 cup"
-perfect_10_recipe[:kosher_salt] = "1/2 tea spoon"
-perfect_10_recipe[:baking_powder] = "1/2 tea spoon"
-perfect_10_recipe[:baking_soda] = "1/4 tea spoon"
-perfect_10_recipe[:xanthan_Gum] = "1/2 tea spoon"
-perfect_10_recipe[:slivered_almonds] = "1/4 cup"
-perfect_10_recipe[:mini_dark_chocolate_chips] = "3/4 cup"
-perfect_10_recipe[:olive_oil] = "1/2 cup"
-perfect_10_recipe[:agave] = "1/4 cu"
+perfect_10_ingredients = {}
+perfect_10_ingredients[:almond_flour] = "2 cups"
+perfect_10_ingredients[:gluten_free_whole_oats] = "1 cup"
+perfect_10_ingredients[:kosher_salt] = "1/2 tea spoon"
+perfect_10_ingredients[:baking_powder] = "1/2 tea spoon"
+perfect_10_ingredients[:baking_soda] = "1/4 tea spoon"
+perfect_10_ingredients[:xanthan_Gum] = "1/2 tea spoon"
+perfect_10_ingredients[:slivered_almonds] = "1/4 cup"
+perfect_10_ingredients[:mini_dark_chocolate_chips] = "3/4 cup"
+perfect_10_ingredients[:olive_oil] = "1/2 cup"
+perfect_10_ingredients[:agave] = "1/4 cu"
+perfect_10_ingredients
 
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
